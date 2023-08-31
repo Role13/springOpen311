877d3b277b6fae1780bc8afc1ab873d2662343f0
Role13 <a345618@uach.mx>
2023-08-31 00:10:04 -0600

    Initial commit

---
+++ src/main/java/com/thincrs/springOpen311/SpringOpen311Application.java
@@ -0,0 +1,13 @@
+package com.thincrs.springOpen311;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpen311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpen311Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/thincrs/springOpen311/SpringOpen311ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thincrs.springOpen311;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpen311ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
